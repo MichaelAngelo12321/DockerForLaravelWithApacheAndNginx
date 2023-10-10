2653a0b51dfca960743de4df3616683994a6e339
Adrian <kemski12321@gmail.com>
2023-10-10 18:29:31 +0100

    Docker with nginx and apache for laravel

---
+++ docker/apache/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+php artisan key:generate
+php artisan migrate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+
+exec docker-php-entrypoint apache2-foreground
+
+
+
---
+++ docker/nginx/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+php artisan key:generate
+php artisan migrate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan serve --port=$PORT --host=0.0.0.0
+
+
+
+
